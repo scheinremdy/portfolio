44ef942f76cf435b5451ffc103fd0b002cbfa049
scheinremdy <remolloschein@gmail.com>
2024-12-10 21:26:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const toggle = document.getElementById('dark-mode-toggle');
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
 });
