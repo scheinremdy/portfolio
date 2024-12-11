84c59d1d650c8f3b235fd1c1d02dd11999aa98b2
scheinremdy <remolloschein@gmail.com>
2024-12-11 10:10:13 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeSwitch = document.querySelector('#dark-mode-toggle');
-    
-    darkModeSwitch.addEventListener('change', () => {
-        document.body.classList.toggle('dark-mode');
-    });
-});
