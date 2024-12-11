301526e305178389bb179e2d09079dd8d8126ce6
scheinremdy <remolloschein@gmail.com>
2024-12-11 21:33:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth Scrolling for anchor links
+// Smooth scrolling functionality for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
