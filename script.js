e335b4604a708de0e6f038b3a97fec92bb1184bd
scheinremdy <remolloschein@gmail.com>
2024-12-11 21:07:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeSwitch = document.querySelector('#dark-mode-toggle');
-    
-    darkModeSwitch.addEventListener('change', () => {
-        document.body.classList.toggle('dark-mode');
+// Smooth Scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
